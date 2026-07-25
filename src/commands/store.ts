a2fa5d94c03ae078a93b04716e8b81e3d0a30c35
levi <cgfeel@gmail.com>
2026-07-25 22:16:03 +0800

    fix: up pr-3

--- src/commands/store.ts
+++ src/commands/store.ts
@@ -1,7 +1,7 @@
 import type { Command } from 'commander'
 import { logger } from '../utils/logger.ts'
 
-// test fix-2
+// test fix-3
 export function registerListCommand(program: Command) {
   program
     .command('store')
