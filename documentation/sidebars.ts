897f10afdef79c86c2dbed304d03f66098d74c71
levi <cgfeel@gmail.com>
2026-08-05 04:58:34 +0800

    docs: update docs

--- documentation/sidebars.ts
+++ documentation/sidebars.ts
@@ -3,6 +3,7 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs'
 const sidebars: SidebarsConfig = {
   docSidebar: [
     'intro',
+    'source',
     {
       type: 'category',
       label: '命令',
@@ -17,6 +18,7 @@ const sidebars: SidebarsConfig = {
         'commands/clear',
         'commands/remove',
         'commands/restore',
+        'commands/mirror',
       ],
     },
     {
