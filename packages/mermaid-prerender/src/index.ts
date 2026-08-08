e94b9a40dd69a862498ebc6f229afb1e24c47357
levi <cgfeel@gmail.com>
2026-08-09 03:33:02 +0800

    fix: up plugin name to MermaidPreRenderPlugin

--- packages/mermaid-prerender/src/index.ts
+++ packages/mermaid-prerender/src/index.ts
@@ -1,3 +1,3 @@
 export { default as loader } from './loader.js'
-export { default as plugin } from './plugin.js'
+export { default as MermaidPreRenderPlugin } from './plugin.js'
 export { hashString } from './utils.js'
