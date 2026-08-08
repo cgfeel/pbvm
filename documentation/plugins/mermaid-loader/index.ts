b53d00c368ba574989069d28437ec12b422d172f
levi <cgfeel@gmail.com>
2026-08-09 03:05:26 +0800

    fix: add mermaid-prerender oicd

--- documentation/plugins/mermaid-loader/index.ts
+++ documentation/plugins/mermaid-loader/index.ts
@@ -1,10 +1,12 @@
 import path from 'node:path'
 import type { Plugin } from '@docusaurus/types'
+// import { plugin as MermaidPreRenderPlugin } from 'mermaid-prerender'
 
 export default function mermaidLoaderPlugin(): Plugin {
   return {
     name: 'mermaid-loader',
     configureWebpack(config) {
+      //   config.plugins?.push([new MermaidPreRenderPlugin()])
       config.module?.rules?.unshift({
         test: /\.mmd$/,
         use: [{ loader: path.resolve(__dirname, 'loader.mjs') }],
---
+++ packages/mermaid-prerender/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.0.1 (2026-08-08)
+
+- Initial release: Mermaid pre-render SVG for build-time optimization
